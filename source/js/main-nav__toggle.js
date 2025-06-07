9f879c4cff2bd65c52e175f89ceee64b56357c99
PalNik <paha-87@mail.ru>
2025-06-07 17:06:47 +1000

    Bug fix

--- source/js/main-nav__toggle.js
+++ source/js/main-nav__toggle.js
@@ -1,5 +1,5 @@
 const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
+const navToggle = navMain.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
